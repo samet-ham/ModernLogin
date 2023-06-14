7966be21d65dd871d15838cf0db60e51ca6d1e1c
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2023-06-14 19:10:55 +0300

    Proje dosyası ekle.

---
+++ ModernLogin/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ModernLogin;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ModernLogin/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ModernLogin;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
