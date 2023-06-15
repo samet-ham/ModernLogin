80d12bec9ce447a95ff1f94086d952ebda43efb5
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2023-06-15 16:28:55 +0300

    edit

--- ModernLogin/Platforms/Android/MainActivity.cs
+++ ModernLogin/Platforms/Android/MainActivity.cs
@@ -1,10 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Color = Android.Graphics.Color;
 
 namespace ModernLogin;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        if(Build.VERSION.SdkInt >= BuildVersionCodes.M)
+        {
+            Window.SetStatusBarColor(Color.ParseColor("#d3d3d3"));
+            Window.SetNavigationBarColor(Color.ParseColor("#d3d3d3"));
+        }
+    }
 }
